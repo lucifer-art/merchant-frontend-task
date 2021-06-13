5eb4bba6f939fe8d7eed732d6c74ae1d8c0a6e33
Shivang Kumar <shivang.kumar@deltaschool.in>
2021-06-13 13:07:29 +0530

    added pagination

--- src/components/CustomerItem/CustomerItem.js
+++ src/components/CustomerItem/CustomerItem.js
@@ -1,5 +1,6 @@
 import { Avatar } from '@material-ui/core';
 import { useHistory, useLocation } from 'react-router-dom';
+import { TableCell,TableRow,Grid,Typography} from '@material-ui/core';
 
 const CustomerItem = props => {
 
@@ -15,15 +16,23 @@ const CustomerItem = props => {
         })
     }
 
-    return <tbody>
-            <tr key={props.merchant.id} onClick={customerDetailHandler} style={{cursor:'pointer'}}>
-                <td><span><Avatar alt="Remy Sharp" src={props.merchant.image_url} /></span><span>{props.merchant.name}</span></td>
-                <td>{props.merchant.email}</td>
-                <td>{props.merchant.phone}</td>
-                <td>{props.merchant.premium}</td>
-                <td>{props.merchant.bids.length && props.bidValue ? props.merchant.bids.sort((a, b) => a.amount - b.amount)[props.merchant.bids.length - 1].amount : props.merchant.bids.length && !props.bidValue ? props.merchant.bids.sort((a, b) => a.amount - b.amount)[0].amount : 0}</td>
-            </tr>
-    </tbody>
+    return <TableRow onClick={customerDetailHandler} style={{ cursor: 'pointer' }}>
+        <TableCell>
+            <Grid container>
+                <Grid item lg={2}>
+                    <Avatar alt="Remy Sharp" src={props.merchant.image_url} />
+                </Grid>
+                <Grid item lg={10}>
+                    <Typography>{props.merchant.name}</Typography>
+                </Grid>
+            </Grid>
+            <span></span><span></span>
+        </TableCell>
+        <TableCell align="left">{props.merchant.email}</TableCell>
+        <TableCell align="left">{props.merchant.phone}</TableCell>
+        <TableCell align="left">{props.merchant.premium}</TableCell>
+        <TableCell align="left">{props.merchant.bids.length && props.bidValue ? props.merchant.bids.sort((a, b) => a.amount - b.amount)[props.merchant.bids.length - 1].amount : props.merchant.bids.length && !props.bidValue ? props.merchant.bids.sort((a, b) => a.amount - b.amount)[0].amount : 0}</TableCell>
+    </TableRow>
 }
 
 export default CustomerItem;
--- src/components/MerchantTable/merchantTable.js
+++ src/components/MerchantTable/merchantTable.js
@@ -5,10 +5,13 @@ import CustomerItem from '../CustomerItem/CustomerItem';
 import classes from './merchantTable.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowDown, faArrowUp } from '@fortawesome/free-solid-svg-icons';
+import { Table,TableBody,TableCell,TableContainer,TableHead,TableRow,TablePagination,Paper} from '@material-ui/core';
 
 const MerchantTable = () => {
     
     const [merchant, setmerchant] = useState([]);
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(5);
     const [bidValue, setbidValue] = useState(true);
     const [asc, setasc] = useState(false);
 
@@ -51,31 +54,54 @@ const MerchantTable = () => {
         setasc(prevState => !prevState)
     }
 
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(+event.target.value);
+        setPage(0);
+    };
+
     let tableContent = <div style={{ position: 'fixed', top: '50%', right: '50%' }}><CircularProgress /></div>;
 
     if (merchant && merchant.length) {
         tableContent = (
-            <table style={{ width: '100%' }} className={classes.customers}>
-            <thead>
-                <tr>
-                    <th>Customer name (with avtar)</th>
-                    <th>Email</th>
-                    <th>Phone</th>
-                    <th>Premium</th>
-                    <th><span style={{cursor:'pointer'}} onClick={sortingHandler}>{bidValue ? 'Max' : 'Min'} bid<span>{asc ? <FontAwesomeIcon icon={faArrowDown} /> : <FontAwesomeIcon icon={faArrowUp} />}</span></span> <span style={{ fontSize: '14px', fontWeight: 'normal' }}>  (Min</span><Switch
-                        checked={bidValue}
-                        onChange={bidChange}
-                        color="primary"
-                        name="checkedB"
-                        inputProps={{ 'aria-label': 'primary checkbox' }}
-                    /><span style={{ fontSize: '14px', fontWeight: 'normal' }}>Max)</span>
-                    </th>
-                </tr>
-            </thead>
-            {testData.map(customer => {
-                return <CustomerItem key={customer.id} merchant={customer} bidValue={bidValue}  />
-            })}
-        </table>
+            <Paper>
+                <TableContainer>
+                    <Table className={classes.customers} aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Customer name (with avtar)</TableCell>
+                                <TableCell align="right">Email</TableCell>
+                                <TableCell align="right">Phone</TableCell>
+                                <TableCell align="right">Premium</TableCell>
+                                <TableCell align="right"><span style={{ cursor: 'pointer' }} onClick={sortingHandler}>{bidValue ? 'Max' : 'Min'} bid<span>{asc ? <FontAwesomeIcon icon={faArrowDown} /> : <FontAwesomeIcon icon={faArrowUp} />}</span></span> <span style={{ fontSize: '14px', fontWeight: 'normal' }}>  (Min</span><Switch
+                                    checked={bidValue}
+                                    onChange={bidChange}
+                                    color="primary"
+                                    name="checkedB"
+                                    inputProps={{ 'aria-label': 'primary checkbox' }}
+                                /><span style={{ fontSize: '14px', fontWeight: 'normal' }}>Max)</span></TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {testData.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(customer => {
+                                return <CustomerItem key={customer.id} merchant={customer} bidValue={bidValue} />
+                            })}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+                <TablePagination align="right"
+                    rowsPerPageOptions={[5, 10, 15]}
+                    component="div"
+                    count={testData.length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    onChangePage={handleChangePage}
+                    onChangeRowsPerPage={handleChangeRowsPerPage}
+                />
+            </Paper>
         )
     }
 
